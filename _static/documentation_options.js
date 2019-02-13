696131c320fd8b12c0cfeaee28dcac9a07fb240d
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-02-13 10:47:04 -0800

    Update docs for INCLUDE_METAL_SOURCES in standalone
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v20180402-166-gc1e8371',
+    VERSION: 'v20180402-175-ge4f2c4b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
